19b896b60cff5c48a5ebcdb25ce7c74f8173c945
Aanjaney Kumar Verma <akuver@Aanjaney.local>
2023-01-06 10:36:05 +0530

    First commit

---
+++ app/src/main/java/com/example/calculator/MainActivity.kt
@@ -0,0 +1,35 @@
+package com.example.calculator
+
+import android.nfc.Tag
+import android.os.Bundle
+import android.util.Log
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.viewModels
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.calculator.ui.theme.CalculatorTheme
+import kotlin.math.log
+
+class MainActivity : ComponentActivity() {
+    private val viewModel by viewModels<CalculatorViewModel>()
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            CalculatorTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    HomeCalculator(calculatorViewModel = viewModel)
+                }
+            }
+        }
+    }
+}
